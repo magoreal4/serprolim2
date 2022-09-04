353e10654571bf7d3efac06cbc0d08e25b43abe1
magoreal <magoreal4@gmail.com>
2022-09-04 00:13:11 -0400

    first commit

--- home/vite.config.js
+++ home/vite.config.js
@@ -4,7 +4,7 @@ const { defineConfig } = require('vite')
 module.exports = defineConfig({
     build: {
         outDir: path.resolve(__dirname, '../static/js'),
-        minify: false,
+        minify: true,
         rollupOptions: {
             output: {
                 entryFileNames: 'home.js',
